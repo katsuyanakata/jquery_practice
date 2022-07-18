941d058805212e808fcc5865cd5aa2d539d3c114
katsuyanakata <postscape.nakata@gmail.com>
2022-07-18 21:56:47 +0900

    <update> swiper

---
+++ swiper/js/function.js
@@ -0,0 +1,19 @@
+
+$(function(){
+  const swiper = new Swiper(".swiper", {
+    loop:true,
+    slidesPerView:1,
+    spaceBetween:100,
+    // ページネーションが必要なら追加
+    pagination: {
+      el: ".swiper-pagination"
+    },
+    // ナビボタンが必要なら追加
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
+
+});
+
